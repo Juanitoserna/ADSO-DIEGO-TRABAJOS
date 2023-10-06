b5450c3de462cbada7819c39414018e80089b4b7
Juanitoserna <sernacastanojuan11@gmail.com>
2023-10-05 20:31:06 -0500

    subiendo guias sql completas

---
+++ TrabajoCompleto-sql/Guia_01/Guia01.sql
@@ -0,0 +1,121 @@
+CREATE DATABASE matricula;
+
+
+--  DATOS DE LA TABLA PROFESORES ---------------------------------------------------
+CREATE TABLE profesores ( 
+dni CHAR(10) PRIMARY KEY, 
+nombre VARCHAR(40), 
+categoria CHAR(4),
+ingreso DATE
+) ENGINE=InnoDB;
+------------------------------------------------------------------------------------
+
+INSERT INTO profesores (dni, nombre, categoria, ingreso)
+VALUES (21111222, 'EVA GOMEZ', 'TEU','1993-10-01');
+
+UPDATE profesores SET nombre = 'EVA PEREZ' WHERE dni = '21111222';
+
+DELETE FROM profesores WHERE dni = '21111222';
+
+
+INSERT INTO profesores (dni, nombre, categoria, ingreso)
+VALUES (21111222 ,'EVA GOMEZ ','TEU' ,'1993-10-01'),
+VALUES (21222333 ,'MANUEL PALOMAR ','TEU' ,'1989-06-16'),
+VALUES (21333444 ,'RAFAEL ROMERO ','ASO6' ,'1992-06-16');
+
+
+--                                DISTINCT
+SELECT DISTINCT categoria FROM profesores;
+
+--                                WHERE
+SELECT nombre FROM profesores WHERE categoria = 'TEU';
+SELECT nombre FROM profesores WHERE categoria = 'TEU' OR categoria = 'ASO6';
+
+--                                ORDER BY
+SELECT creditos, descripcion FROM asignaturas ORDER BY creditos;
+SELECT creditos, descripcion FROM asignaturas WHERE creditos > 4.5 ORDER BY creditos DESC;
+SELECT creditos, descripcion FROM asignaturas ORDER BY creditos, descripcion;
+
+--                                 NULL (NULOS)
+SELECT * FROM asignaturas WHERE creditosp IS NULL;
+
+--                                 CONSTANTES
+SELECT 'La asignatura ', descripcion, ' tiene ', creditos, ' créditos' FROM asignaturas ORDER BY creditos;
+
+--                                 SELECCION DE MAS DE UNA TABLA
+SELECT * FROM asignaturas, profesores, imparte;
+SELECT * FROM asignaturas, profesores, imparte WHERE profesores.dni = imparte.dni AND asignatura = codigo;
+SELECT nombre, descripcion FROM asignaturas, profesores, imparte WHERE profesores.dni=imparte.dni AND asignatura=codigo;
+
+--                                  EXPRESION DE SELECCCION DE FILAS
+SELECT creditos, descripción FROM asignaturas WHERE creditos BETWEEN 5 AND 8;
+SELECT descripcion FROM asignaturas WHERE codigo in ('FBD', 'DGBD');
+SELECT nombre FROM profesores p, imparte i WHERE p.dni = i.dni AND asignatura NOT IN ('HI', 'FBD', 'DGBD');
+
+--                                  SUBCADENAS DE CARACTERES
+SELECT * FROM profesores WHERE nombre LIKE 'RAFA%';
+SELECT codigo FROM asignaturas WHERE descripcion LIKE '%BASES DE DATOS%';
+SELECT codigo FROM asignaturas WHERE codigo LIKE ' ';
+SELECT descripcion FROM asignaturas WHERE descripcion LIKE '%INFORMATIC_';
+
+--                                  CONSULTAS ANIDADAS
+SELECT descripcion, creditos FROM asignaturas WHERE creditos = (SELECT MIN(creditos) FROM asignaturas);
+SELECT descripcion, creditos FROM asignaturas WHERE creditos = (SELECT MIN(creditos) FROM asignaturas);
+SELECT * FROM asignaturas WHERE creditos >(SELECT creditos FROM asignaturas WHERE codigo = 'HI');
+SELECT descripcion FROM asignaturas WHERE creditos >= ALL (SELECT creditos FROM asignaturas);
+SELECT descripcion FROM asignaturas WHERE creditos = (SELECT MAX(creditos) FROM asignaturas);
+SELECT descripcion FROM asignaturas WHERE creditos > ANY (SELECT creditos FROM asignaturas);
+SELECT descripcion FROM asignaturas WHERE creditos != (SELECT MIN(creditos) FROM asignaturas);
+SELECT descripcion FROM asignaturas WHERE creditos != (SELECT MIN(creditos) FROM asignaturas);
+SELECT descripcion FROM asignaturas WHERE creditos != (SELECT MIN(creditos) FROM asignaturas);
+SELECT p.* FROM profesores p, imparte i WHERE p.dni = i.dni;
+
+
+
+
+--  DATOS DE LA TABLA ASIGNATURAS --------------------------------------------------
+CREATE TABLE asignaturas ( 
+codigo char(5) PRIMARY KEY, 
+descripcion varchar(35), 
+creditos float(3,1),
+creditosp float(3,1),
+) ENGINE=InnoDB;
+------------------------------------------------------------------------------------
+
+INSERT INTO asignaturas (codigo, descripcion, creditos, creditosp)
+VALUES ('DGBD', 'DISEÑO Y GESTIÓN DE BASES DE DATOS', 6.0, 3.0);
+
+INSERT INTO asignaturas (codigo, descripcion, creditos, creditosp)
+VALUES ('DGBD' ,'DISEÑO Y GESTION DE BASES DE DATOS' 6.0 ,3.0),
+VALUES ('FBD', 'FUNDAMENTOS DE LAS BASES DE DATOS' ,6.0 ,1.5),
+VALUES ('FP', 'FUNDAMENTOS DE LA PROGRAMACION', 9.0 ,4.5),
+VALUES ('HI', 'HISTORIA DE LA INFORMATICA' ,4.5 , null),
+VALUES ('PC' ,'PROGRAMACION CONCURRENTE' ,6.0 ,1.5);
+
+
+UPDATE asignaturas SET codigo = 'AAA' WHERE codigo = 'DGBD';
+
+DELETE FROM asignaturas WHERE codigo = 'AAA';
+DELETE FROM asignaturas WHERE codigo = 'DGBD'; 
+
+
+--  DATOS DE LA TABLA IMPARTE ------------------------------------------------------
+CREATE TABLE imparte ( 
+dni CHAR(10), 
+asignatura CHAR(5),
+PRIMARY KEY (dni, asignatura),
+FOREIGN KEY (dni) REFERENCES profesores (dni),
+FOREIGN KEY (asignatura) REFERENCES asignaturas (codigo)
+) ENGINE=InnoDB;
+------------------------------------------------------------------------------------ 
+
+INSERT INTO imparte (dni, asignatura) 
+VALUES (21111222, 'DGBD');
+
+INSERT INTO imparte (dni, asignatura) 
+VALUES (21111222 ,'DGBD'),
+VALUES (21111222 ,'FBD'),
+VALUES (21333444 ,'PC');
+
+
+
---
+++ TrabajoCompleto-sql/Guia_03/Guia03.sql
@@ -0,0 +1,67 @@
+--                    Reunión (join)
+--                    Tipos de reunión
+--      Equijoin
+SELECT * FROM asignaturas, profesores, imparte
+WHERE profesores.dni = imparte.dni AND asignatura = código;
+
+--      Self join
+SELECT i1.dni, ' imparte la misma asignatura que ', i2.dni
+FROM imparte i1, imparte i2
+WHERE i1.asignatura= i2.asignatura;
+
+--            Producto cartesiano (consume muchos recursos)
+SELECT dni, codigo
+FROM profesores, asignaturas;
+
+--                 Inner join, simple join
+SELECT nombre, descripcion FROM asignaturas
+JOIN imparte ON (codigo=asignatura)
+JOIN profesores ON (imparte.dni=profesores.dni);
+
+SELECT nombre, descripcion
+FROM asignaturas JOIN imparte ON (codigo=asignatura)
+JOIN profesores ON (imparte.dni=profesores.dni)
+WHERE descripcion NOT LIKE 'PROGRAMACION%';
+--      Outer join
+SELECT p.*, i.asignatura FROM profesores p
+LEFT JOIN imparte i ON (p.dni=i.dni);
+
+--      Antijoin
+SELECT * from profesores
+WHERE dni NOT IN (SELECT dni FROM imparte);
+
+--      Semijoin
+SELECT * FROM profesores p
+WHERE EXISTS (SELECT * FROM imparte i WHERE i.dni = p.dni);
+
+--        NUEVA TABLA PARA LA BASE DE DATOS MATRICULA
+CREATE TABLE coordinadores (
+ dni VARCHAR(10) PRIMARY KEY,
+ nombre VARCHAR(40),
+ dpto CHAR(4),
+ asig CHAR(5),
+FOREIGN KEY (asignatura) REFERENCES asignaturas (codigo)
+);
+
+--       CREAMOS NYUEVOS DATOS PARA LA TABLA COORDINADORES
+INSERT INTO coordinadores (dni, nombre, dpto, asig)
+VALUES ('55777666' ,'AGAPITO CIFUENTES' 'DLSI' ,'FP'),
+VALUES ('66555444', 'ROMUALDO GOMEZ' ,'DLSI' ,'HI'),
+VALUES ('99222111', 'CATURLO PEREZ', 'DLSI' ,null);
+
+--  Muestra todos los coordinadores y, si lo hacen, las asignaturas que coordinan.
+SELECT * FROM coordinadores
+LEFT JOIN asignaturas ON (codigo=asig);
+
+--  Muestra los coordinadores que tienen asignatura y todas las asignaturas.
+SELECT * FROM coordinadores RIGHT
+JOIN asignaturas ON (codigo=asig);
+
+--  Muestra todos los coordinadores y todas las asignaturas y si hay relación entre ellos.
+SELECT * FROM coordinadores FULL
+JOIN asignaturas ON (codigo=asig);
+
+
+   
+   
+  
